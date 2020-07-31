ba931e82395104a4fff0044ece10a18058ab18af
Robert Pearce <me@robertwpearce.com>
2020-07-31 09:40:13 -0400

    rm namedExports from rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-import React from 'react'
-import reactDom from 'react-dom'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript2'
@@ -9,10 +7,6 @@ import pkg from './package.json'
 
 const cjsConfig = {
   include: /node_modules/,
-  namedExports: {
-    react: Object.keys(React),
-    'react-dom': Object.keys(reactDom)
-  }
 }
 
 const isExternal = id => !id.startsWith('.') && !id.startsWith('/')
